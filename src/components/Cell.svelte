f9661bdd460a7b3e3e4091eccb575e9f9f96aa72
Victor Broman <victor@X230>
2022-04-13 11:42:12 +0200

    Adds grid and cell component with edit state

---
+++ src/components/Cell.svelte
@@ -0,0 +1,37 @@
+<script type="text/javascript" lang="ts">
+ export let content: string = '';
+ export let state: string;
+ export let callback;
+
+ let value = content;
+ function updateCell(newValue): void {
+     content = newValue;
+     state = "idle";
+     callback();
+ }
+
+</script>
+
+<div class="cell">
+    {#if (state == 'edit')}
+        <input autofocus on:blur={updateCell(value)} bind:value={value}/>
+    {:else if (state == 'idle')}
+        <div on:click="{() => state = "edit"}">
+        {content}
+        </div>
+    {/if}
+</div>
+
+<style>
+ .cell {
+     border-style: solid;
+     border-width: 1px;
+     height: 1.5em;
+     min-width: 5em;
+     margin: 0;
+     padding: 0;
+ }
+ input {
+     max-width: 5em;
+ }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,65 @@
+<script type="text/javascript" lang="ts">
+	import Cell from '../components/Cell.svelte';
+
+	type Row = number;
+	type Column = string;
+
+	type Position = {
+		row: Row;
+		column: Column;
+	};
+
+	type State = {
+		rows: Row[];
+		columns: Column[];
+		Cells: Map<Position, Cell>;
+	};
+
+  type Cell = {
+    value: number;
+    function: string;
+    state: CellState;
+  }
+
+  enum CellState {'idle','edit'}
+
+	//initiate state
+	let state: State = {
+		rows: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+		columns: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+		Cells: new Map()
+	};
+
+  function evaluate(position: Position): number  {
+    console.log(position)
+    return 1
+  }
+
+  
+
+</script>
+
+<table>
+	<tr>
+		<th />
+		{#each state.columns as heading}
+			<th>{heading}</th>
+		{/each}
+	</tr>
+	{#each state.rows as row}
+		<tr>
+			<th>{row}</th>
+			{#each state.columns as column}
+				<td>
+					<Cell content="." state="idle" callback={() => evaluate({row, column})} />
+				</td>
+			{/each}
+		</tr>
+	{/each}
+</table>
+
+<style type="text/css" media="screen">
+	td {
+		padding: 0;
+	}
+</style>
